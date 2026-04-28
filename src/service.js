8a6ffea5668017708a1891f8780b8c34e24d2a6a
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-04-29 00:00:27 +0530

    feat: add request validation

--- src/service.js
+++ src/service.js
@@ -1,9 +1,10 @@
-class DemoService {
-  constructor() { this.counter = 0; }
-  increment() { return ++this.counter; }
-  decrement() { return --this.counter; }
-  reset() { this.counter = 0; }
-  // auto-commit: 1777401025686
+const EventEmitter = require('events');
+
+class DemoService extends EventEmitter {
+  constructor() { super(); this.queue = []; }
+  enqueue(job) { this.queue.push(job); this.emit('job', job); }
+  dequeue() { return this.queue.shift(); }
+  // auto-commit: 1777401027252
 }
 
 module.exports = DemoService;
