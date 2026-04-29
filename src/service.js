b9c4715000e878d2b1ed48215b0254d9888ec854
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-04-29 12:03:13 +0530

    feat: add health check endpoint

--- src/service.js
+++ src/service.js
@@ -1,13 +1,9 @@
-const crypto = require('crypto');
-
 class DemoService {
-  hash(data) {
-    return crypto.createHash('sha256').update(data).digest('hex');
-  }
-  verify(data, hash) {
-    return this.hash(data) === hash;
-  }
-  // auto-commit: 1777444390711
+  constructor() { this.items = []; }
+  add(item) { this.items.push(item); }
+  getAll() { return [...this.items]; }
+  clear() { this.items = []; }
+  // auto-commit: 1777444392315
 }
 
 module.exports = DemoService;
