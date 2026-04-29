18427f7fe77adeaea6483b69398ab28941f2e84b
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-04-29 12:03:42 +0530

    fix: resolve race condition

--- src/service.js
+++ src/service.js
@@ -1,10 +1,9 @@
-const EventEmitter = require('events');
-
-class DemoService extends EventEmitter {
-  constructor() { super(); this.queue = []; }
-  enqueue(job) { this.queue.push(job); this.emit('job', job); }
-  dequeue() { return this.queue.shift(); }
-  // auto-commit: 1777444420220
+class DemoService {
+  constructor() { this.counter = 0; }
+  increment() { return ++this.counter; }
+  decrement() { return --this.counter; }
+  reset() { this.counter = 0; }
+  // auto-commit: 1777444421740
 }
 
 module.exports = DemoService;
