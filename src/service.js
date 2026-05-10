d1a65d68e49425df3ee5b99eaa28ab451b4c2d06
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:31:34 +0530

    feat: add rate limiting

--- src/service.js
+++ src/service.js
@@ -1,9 +1,10 @@
-class DemoService {
-  constructor() { this.counter = 0; }
-  increment() { return ++this.counter; }
-  decrement() { return --this.counter; }
-  reset() { this.counter = 0; }
-  // auto-commit: 1778396492574
+const EventEmitter = require('events');
+
+class DemoService extends EventEmitter {
+  constructor() { super(); this.queue = []; }
+  enqueue(job) { this.queue.push(job); this.emit('job', job); }
+  dequeue() { return this.queue.shift(); }
+  // auto-commit: 1778396493993
 }
 
 module.exports = DemoService;
