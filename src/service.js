ffa265499d8386480fbe991ec8f2427b3d841391
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:23:02 +0530

    feat: implement caching layer

--- src/service.js
+++ src/service.js
@@ -1,10 +1,9 @@
-const EventEmitter = require('events');
-
-class DemoService extends EventEmitter {
-  constructor() { super(); this.queue = []; }
-  enqueue(job) { this.queue.push(job); this.emit('job', job); }
-  dequeue() { return this.queue.shift(); }
-  // auto-commit: 1778395980364
+class DemoService {
+  constructor() { this.counter = 0; }
+  increment() { return ++this.counter; }
+  decrement() { return --this.counter; }
+  reset() { this.counter = 0; }
+  // auto-commit: 1778395981717
 }
 
 module.exports = DemoService;
