625161bf77633a0bf487fe3942173a39f3a07cfa
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-11 02:30:22 +0530

    feat: implement caching layer

--- src/service.js
+++ src/service.js
@@ -1,13 +1,9 @@
-const crypto = require('crypto');
-
 class DemoService {
-  hash(data) {
-    return crypto.createHash('sha256').update(data).digest('hex');
-  }
-  verify(data, hash) {
-    return this.hash(data) === hash;
-  }
-  // auto-commit: 1778406424644
+  constructor() { this.items = []; }
+  add(item) { this.items.push(item); }
+  getAll() { return [...this.items]; }
+  clear() { this.items = []; }
+  // auto-commit: 1778446821876
 }
 
 module.exports = DemoService;
