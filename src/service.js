40b69c79a2f4f1c244196903c3724ca49eb76647
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:45:48 +0530

    chore: update dependencies

--- src/service.js
+++ src/service.js
@@ -1,10 +1,13 @@
-const { randomUUID } = require('crypto');
-
 class DemoService {
-  constructor() { this.store = new Map(); }
-  set(key, val) { this.store.set(key, val); }
-  get(key) { return this.store.get(key); }
-  // auto-commit: 1778397198678
+  chunk(arr, size) {
+    const chunks = [];
+    for (let i = 0; i < arr.length; i += size) chunks.push(arr.slice(i, i + size));
+    return chunks;
+  }
+  flatten(arr) {
+    return arr.flat(Infinity);
+  }
+  // auto-commit: 1778397347486
 }
 
 module.exports = DemoService;
