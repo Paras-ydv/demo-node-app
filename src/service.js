ba86e65ebf93031f010bffe58397ab392a679d8b
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:43:14 +0530

    chore: update build scripts

--- src/service.js
+++ src/service.js
@@ -1,13 +1,10 @@
-const crypto = require('crypto');
+const EventEmitter = require('events');
 
-class DemoService {
-  hash(data) {
-    return crypto.createHash('sha256').update(data).digest('hex');
-  }
-  verify(data, hash) {
-    return this.hash(data) === hash;
-  }
-  // auto-commit: 1778397192974
+class DemoService extends EventEmitter {
+  constructor() { super(); this.queue = []; }
+  enqueue(job) { this.queue.push(job); this.emit('job', job); }
+  dequeue() { return this.queue.shift(); }
+  // auto-commit: 1778397194373
 }
 
 module.exports = DemoService;
