5d0b93f2e24a568a41c23f8e3955f2accffb7652
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 14:51:56 +0530

    feat: implement authentication

--- src/service.js
+++ src/service.js
@@ -1,10 +1,14 @@
-const EventEmitter = require('events');
-
-class DemoService extends EventEmitter {
-  constructor() { super(); this.queue = []; }
-  enqueue(job) { this.queue.push(job); this.emit('job', job); }
-  dequeue() { return this.queue.shift(); }
-  // auto-commit: 1778404401473
+class DemoService {
+  filter(arr, predicate) {
+    return arr.filter(predicate);
+  }
+  map(arr, fn) {
+    return arr.map(fn);
+  }
+  reduce(arr, fn, init) {
+    return arr.reduce(fn, init);
+  }
+  // auto-commit: 1778404915887
 }
 
 module.exports = DemoService;
