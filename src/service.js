8abe8cf6e2b24c1f207c002dfb574150cb2854bd
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:54:56 +0530

    chore: update dependencies

--- src/service.js
+++ src/service.js
@@ -1,13 +1,9 @@
 class DemoService {
-  chunk(arr, size) {
-    const chunks = [];
-    for (let i = 0; i < arr.length; i += size) chunks.push(arr.slice(i, i + size));
-    return chunks;
-  }
-  flatten(arr) {
-    return arr.flat(Infinity);
-  }
-  // auto-commit: 1778397893926
+  constructor() { this.counter = 0; }
+  increment() { return ++this.counter; }
+  decrement() { return --this.counter; }
+  reset() { this.counter = 0; }
+  // auto-commit: 1778397895581
 }
 
 module.exports = DemoService;
