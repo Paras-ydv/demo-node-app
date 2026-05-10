2ab8d0698737a2087d22d18123bf69950f3df990
Paras <164542355+Paras-ydv@users.noreply.github.com>
2026-05-10 12:54:26 +0530

    chore: update build scripts

--- src/service.js
+++ src/service.js
@@ -1,14 +1,9 @@
 class DemoService {
-  filter(arr, predicate) {
-    return arr.filter(predicate);
-  }
-  map(arr, fn) {
-    return arr.map(fn);
-  }
-  reduce(arr, fn, init) {
-    return arr.reduce(fn, init);
-  }
-  // auto-commit: 1778397841226
+  constructor() { this.items = []; }
+  add(item) { this.items.push(item); }
+  getAll() { return [...this.items]; }
+  clear() { this.items = []; }
+  // auto-commit: 1778397865713
 }
 
 module.exports = DemoService;
